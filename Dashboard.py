639811bf4573b2ced3ede2a16a12e0a50d895e5f
Lin HE <104896569+Lin0He@users.noreply.github.com>
2024-05-27 01:08:10 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -2,9 +2,7 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('psd_coffee.csv')
-
-
+df = pd.read_csv('https://raw.githubusercontent.com/Lin0He/Dashboard/master/psd_coffee.csv')
 
 # Aggregate production data by country
 production_by_country = df.groupby("Country")["Production"].sum().reset_index()
