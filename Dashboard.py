812d3cdcf9c490c36a9077cc4f2e99309feaccd4
Lin HE <104896569+Lin0He@users.noreply.github.com>
2024-05-27 01:59:12 +0200

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -134,4 +134,4 @@ def update_graph(country_value, indicate_selection, indicate_selection0):
     return fig, fig0
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=False)
